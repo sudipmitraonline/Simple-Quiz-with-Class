a2cd8e5b40207758922cfdde8bb74e4f6ab627f4
Sudip Mitra <44464604+sudipmitraonline@users.noreply.github.com>
2020-04-21 16:03:36 +0530

    update

--- main.py
+++ main.py
@@ -1,103 +1,28 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 5,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdin",
-     "output_type": "stream",
-     "text": [
-      "What is the fastest programming language among these ? \n",
-      " a) Java \n",
-      " b) Python \n",
-      " c) C++\n",
-      "\n",
-      "Your Answer : a\n",
-      "\n",
-      "Tensorflow is a module of ? \n",
-      " a) Java \n",
-      " b) Python \n",
-      " c) C++\n",
-      "\n",
-      "Your Answer : x\n",
-      "\n",
-      "Suitable language for android deevelopment ? \n",
-      " a) Java \n",
-      " b) Python \n",
-      " c) C++\n",
-      "\n",
-      "Your Answer : c\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "\n",
-      "You got 0 out of 3 correct.\n"
-     ]
-    }
-   ],
-   "source": [
-    "class Question:\n",
-    "    def __init__(self, prompt, answer):\n",
-    "        self.prompt = prompt\n",
-    "        self.answer = answer\n",
-    "\n",
-    "#@Author : Sudip Mitra\n",
-    "#E-mail : sudipmitraonline@gmail.com\n",
-    "\n",
-    "question_prompts = [\n",
-    "    \"What is the fastest programming language among these ? \\n a) Java \\n b) Python \\n c) C++\\n\\nYour Answer :\",\n",
-    "    \"\\nTensorflow is a module of ? \\n a) Java \\n b) Python \\n c) C++\\n\\nYour Answer :\",\n",
-    "    \"\\nSuitable language for android deevelopment ? \\n a) Java \\n b) Python \\n c) C++\\n\\nYour Answer :\",\n",
-    "]\n",
-    "\n",
-    "questions = [\n",
-    "    Question(question_prompts[0],\"c\"),\n",
-    "    Question(question_prompts[1],\"b\"),\n",
-    "    Question(question_prompts[2],\"a\"),\n",
-    "]\n",
-    "\n",
-    "def run_test(questions):\n",
-    "    score = 0\n",
-    "    for question in questions:\n",
-    "        answer = input(question.prompt)\n",
-    "        if answer == question.answer :\n",
-    "            score += 1\n",
-    "    print(\"\\nYou got \" + str(score) + \" out of \" + str(len(questions)) + \" correct.\")\n",
-    "run_test(questions)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.0"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
+#@Author : Sudip Mitra
+#E-mail : sudipmitraonline@gmail.com
+
+question_prompts = [
+    "What is the fastest programming language among these ? \n a) Java \n b) Python \n c) C++\n\nYour Answer :",
+    "\nTensorflow is a module of ? \n a) Java \n b) Python \n c) C++\n\nYour Answer :",
+    "\nSuitable language for android deevelopment ? \n a) Java \n b) Python \n c) C++\n\nYour Answer :",
+]
+
+questions = [
+    Question(question_prompts[0],"c"),
+    Question(question_prompts[1],"b"),
+    Question(question_prompts[2],"a"),
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer :
+            score += 1
+    print("\nYou got " + str(score) + " out of " + str(len(questions)) + " correct.")
+run_test(questions)
